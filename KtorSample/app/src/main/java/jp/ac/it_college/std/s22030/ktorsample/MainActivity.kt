cc2700d1cbf84db4a9811e11c27e1fe00cc51d6c
s22030 <s22030@std.it-college.ac.jp>
2023-10-10 12:23:12 +0900

    ktorクライアントを使ったサンプル

--- KtorSample/app/src/main/java/jp/ac/it_college/std/s22030/ktorsample/MainActivity.kt
+++ KtorSample/app/src/main/java/jp/ac/it_college/std/s22030/ktorsample/MainActivity.kt
@@ -2,8 +2,38 @@ package jp.ac.it_college.std.s22030.ktorsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.UiThread
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.url
+import io.ktor.serialization.kotlinx.json.json
 import jp.ac.it_college.std.s22030.ktorsample.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s22030.ktorsample.model.WeatherInfo
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+
+private val ktorClient = HttpClient(CIO){
+    engine {
+        endpoint{
+            connectTimeout = 5000
+            requestTimeout = 5000
+            socketTimeout = 5000
+        }
+    }
+    install(ContentNegotiation){
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }
+        )
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -20,8 +50,27 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvCityList.apply {
             adapter = CityAdapter {
+                getWeatherInfo(it.q)
             }
             layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    @UiThread
+    private fun getWeatherInfo(q: String){
+        lifecycleScope.launch {
+            val url = "$WEATHER_INFO_URL&q=$q&appid=$APP_ID"
+            val result = ktorClient.get {
+                url(url)
+            }.body<WeatherInfo>()
+
+            binding.tvWeatherTelop.text = getString(R.string.tv_telop, cityName)
+            binding.tvWeatherDesc.text = getString(
+                R.string.tv_desc,
+                weather[0].description,
+                coordinates.longitude,
+                cooredinates.latitude
+            )
         }
     }
 }
